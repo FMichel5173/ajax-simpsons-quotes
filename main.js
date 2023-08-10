1a8d66a0a666182fe0b8001b1d57f3b2649d325c
FMichel5173 <fmichel81@sfr.fr>
2023-08-10 18:19:08 +0200

    Ready to fetch the API and Display quotes

---
+++ main.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Retrieval of a quote
+  function getQuote() {
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+      .then(function (response) {
+        const quote = response.data[0];
+        displayQuote(quote);
+      });
+  }
+
+  // Display of the quote
+  function displayQuote(quote) {
+    const quoteHtml = `
+          <div>
+              <img src="${quote.image}" alt="${quote.character}" />
+              <p><strong>${quote.character}</strong>: ${quote.quote}</p>
+          </div>
+      `;
+    document.getElementById("quote").innerHTML = quoteHtml;
+  }
+
+  // Initial loading
+  getQuote();
+
+  // Reload a new quote with a click
+  document.getElementById("loadQuote").addEventListener("click", getQuote);
+});
